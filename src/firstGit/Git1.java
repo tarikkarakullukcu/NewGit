dd9ec52ce2237ea9a7ca7836d039167375d1bd2b
Tarik <tarikkarakullukcu@gmail.com>
2021-04-27 13:03:17 +0300

    V2

--- src/firstGit/Git1.java
+++ src/firstGit/Git1.java
@@ -4,6 +4,10 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		System.out.println("Version 1");
+		System.out.println("My first commit");
+		System.out.println("My second commit");
+		System.out.println("My third commit");
+		
 
 	}
 
